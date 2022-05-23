e10e8597280318e402c89a697a8065127392bb08
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2022-05-23 20:37:40 +0200

    Changes and Development: - Truncate post content on HomePage - Read more link to show the full content on post page"

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var _ = require('lodash');
 
 
 // ------------------ lurespam Data -----------------
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+let homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -25,14 +25,15 @@ app.use(express.static("public"));
 // ------------------- Variables -------------------
 
 let posts =[];
-
+let postID =[];
 
 
 // -------------------- Application -----------------
 app.get("/", (req, res)=>{
   res.render("home", {
     homeStartingContent: homeStartingContent,
-    posts: posts
+    posts: posts,
+
   });
 
 })
@@ -45,24 +46,34 @@ app.get("/contact", (req, res)=>{
   res.render("contact", {contactContent: contactContent});
 })
 
-app.get("/compose", (req, res)=>{
-  res.render("compose");
-})
 
 // Dynamic URL
 app.get("/post/:postID", (req, res)=>{
+
+  // Matching URL with post title
 const postID = _.lowerCase(req.params.postID);
   posts.forEach(post=>{
     const title = _.lowerCase(post.title);
     if(postID === title){
       console.log("Matched");
+      res.render('post', {
+        title: post.title,
+        content:post.content,
+      });
+
     }else{
       console.log("Something is wrong!");
     }
+    // I dont like the logic above, its not fast enough! with extra checking
   });
 })
 
 
+
+
+app.get("/compose", (req, res)=>{
+  res.render("compose");
+})
 
 app.post("/compose", (req, res)=>{
 
@@ -75,6 +86,8 @@ app.post("/compose", (req, res)=>{
 })
 
 
+
+// -------------------- Functions -----------------
 
 
 
